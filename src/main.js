7d746df497f34f64aa347ff776b1efb7a513163c
Vitor Adonai <vitoradonaiarruda@gmail.com>
2017-11-12 16:48:54 -0200

    Login 1/32: Start

--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ import VueResource from 'vue-resource'
 Vue.config.productionTip = false
 Vue.use(VueResource)
 alertify.defaults.notifier.position = 'top-right'
+Vue.http.interceptors.push(function (request, next) {
+    if(request.url[0] === '/') {
+      request.url = process.env.API + request.url
+    }
+    next(function (response) {
+        if(response.status == 422) {
+            response.body.errors.forEach(function (e) {
+                alertify.error(e)
+            })
+        }
+    })
+})
 
 /* eslint-disable no-new */
 new Vue({
